015ae1ef55a5c22de7281f039af73a98a3d46d77
MD. MAHDI HASAN <111120793+mdmahdihasan50@users.noreply.github.com>
2023-06-30 22:06:09 -0700

    Update Todo.dart

--- lib/Fragment/Todo.dart
+++ lib/Fragment/Todo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_class/main.dart';
 
 class Todo extends StatefulWidget {
   const Todo({Key? key}) : super(key: key);
@@ -42,64 +43,81 @@ class _TodoState extends State<Todo> {
                     Expanded(
                       flex: 2,
                         child: ElevatedButton(onPressed: () {
-                          setState(() {
+
+                          if(Todolistadd.text.trim().isNotEmpty){
                             Todolist.add(Todolistadd.text);
                             Todolistadd.clear();
-                          });
+
+                            if(mounted){
+                              setState(() {});
+                            }
+
+                          }
+                          else{
+                            toast('কিছু লিখুন', Colors.green);
+                          }
+
                           }, child: Text('add'))),
 
               ]),
             ),
-            SizedBox(
+            const SizedBox(
               height: 20,
             ),
             Expanded(
               child: ListView.builder(
                 itemCount: Todolist.length,
                 itemBuilder: (context, index) {
-                return Card(
-                  child: Row(
-                      children: [
-                        SizedBox(width: 10,),
-                        Expanded(flex: 8,
-                            child: Text(Todolist[index])),
-                        Expanded(
-                            flex: 2,
-                            child: IconButton(onPressed: () {
+                return GestureDetector(
+                  onLongPress: () {
+
+                    showDialog(context: context, builder: (context) {
+                      return AlertDialog(
+
+                        icon: Icon(Icons.delete),
+                        title: Text('sure remove ?'),
+                        actionsAlignment: MainAxisAlignment.center,
+                        actions: [
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                  fixedSize: Size(100, 50)),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              }, child: Text('NO')),
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                  fixedSize: Size(100, 50)),
+                              onPressed: () {
+                                Navigator.pop(context);
+                                Todolist.removeAt(index);
+
+                                if(mounted){
+                                  setState(() {});
+                                }
+
+                              }, child: Text('Yes')),
+                        ],);
+                    },);
+
+                  },
+                  child: Card(
+                    child: Row(
+                        children: [
+                          SizedBox(width: 10,),
+                          Expanded(flex: 8,
+                              child: Text(Todolist[index])),
+                          Expanded(
+                              flex: 2,
+                              child: IconButton(onPressed: () {
 
 
-                              showDialog(context: context, builder: (context) {
-                                return AlertDialog(
 
-                                  icon: Icon(Icons.delete),
-                                  title: Text('sure remove ?'),
-                                  actionsAlignment: MainAxisAlignment.center,
-                                  actions: [
-                                    ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                            fixedSize: Size(100, 50)),
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        }, child: Text('NO')),
-                                    ElevatedButton(
-                                        style: ElevatedButton.styleFrom(
-                                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                            fixedSize: Size(100, 50)),
-                                        onPressed: () {
-                                          Navigator.pop(context);
-                                          setState(() {
-                                            Todolist.removeAt(index);
-                                          });
+                              }, icon: Icon(Icons.edit)),
 
-                                        }, child: Text('Yes')),
-                                  ],);
-                              },);
-
-
-                            }, icon: Icon(Icons.delete)),
-
-                        )]),
+                          )]),
+                  ),
                 );
               },),
             ),
