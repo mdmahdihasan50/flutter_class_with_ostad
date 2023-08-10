07a0399885d464f41b786dd60dc77d2157073061
MD. MAHDI HASAN <111120793+mdmahdihasan50@users.noreply.github.com>
2023-08-10 15:02:44 +0600

    ok

--- lib/Fragment/HomeFagment.dart
+++ lib/Fragment/HomeFagment.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import '../Screen/RestAPI.dart';
 import '../Screen/BuySelCal.dart';
+import '../main.dart';
 
 class HomeFagment extends StatelessWidget {
-  const HomeFagment({Key? key}) : super(key: key);
+  HomeFagment({Key? key}) : super(key: key);
+
+  TextEditingController _vid=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -82,7 +85,66 @@ class HomeFagment extends StatelessWidget {
                 shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
                 fixedSize: const Size(300, 50)),
             onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => BuySelCal(),));
+
+              showDialog(
+                barrierDismissible: false,
+                context: context, builder: (context) {
+                return AlertDialog(
+                  titlePadding: const EdgeInsets.only(left: 16),
+                  contentPadding: const EdgeInsets.only(left: 8,right: 8,bottom: 8),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text('Chose an Action'),
+                      //const Spacer(),
+                      IconButton(onPressed: () {
+                        Navigator.pop(context);
+                      }, icon: const Icon(Icons.cancel_outlined))
+                    ],
+                  ),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: [
+                            TextFormField(validator: (value) {
+                              if(value!.isEmpty){
+                                return 'Write Your Password';
+                              }
+                            }, keyboardType: TextInputType.text,controller: _vid, decoration: InputDecoration(prefixIcon: const Icon(Icons.play_circle),label: const Text('Write Your Password'),border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)))),
+                            const SizedBox(height: 10),
+                            SizedBox(
+                              width: double.infinity,
+                              child: ElevatedButton(style: ElevatedButton.styleFrom(fixedSize: const Size(double.infinity, 20)),onPressed: () {
+
+                              //  Navigator.push(context, MaterialPageRoute(builder: (context) => BuySelCal(),));
+
+                                var vid=_vid.text.trim();
+
+                                RegExp regex = RegExp("90905350");
+                                if (regex.hasMatch(vid) && vid.length==8) {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) => BuySelCal(),));
+                                  _vid.clear();
+                                } else {
+
+                                  toast('Enter valid password', Colors.red);
+                                }
+
+
+
+                              }, child: const Text("Submit")),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },);
+
+
             }, child: const Text('Buy sell Calculate')),
       ),
       Padding(
